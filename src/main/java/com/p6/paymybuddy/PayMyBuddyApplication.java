50b92840e9c898d28e8a429b96b4306100c367d9
CILL-PC <gmooocgel@gmail.com>
2021-11-25 16:27:07 +0100

    SpringBoot basics : add file entity

--- src/main/java/com/p6/paymybuddy/PayMyBuddyApplication.java
+++ src/main/java/com/p6/paymybuddy/PayMyBuddyApplication.java
@@ -3,11 +3,35 @@ package com.p6.paymybuddy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.transaction.Transactional;
+
 @SpringBootApplication
 public class PayMyBuddyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PayMyBuddyApplication.class, args);
     }
+
+
+    /*
+    *  L'annotation Transactional permet de s'assurer les données sont enregistrés en cache pour que les méthodes save
+    *  Ainsi, il exécutrera tout en même temps.
+    *
+    * Un bon SQL est un SQL que tu peux exécuter plusieurs fois.
+    */
+
+
+//    @Transactional
+//    public void doTransaction(Long idCreditor, Long idDebitor, double amount) {
+//        User credit = userRepository.findById(idCreditor);
+//        User debit = userRepository.findById(idDebitor);
+//        double commission = amount * 0.05;
+//
+//        credit.setAmount(credit.getAmount() + amount);
+//        debit.setAmount(debit.getAmount() - amount - commission);
+//        userRepository.save(credit);
+//        userRepository.save(debit);
+//        transactionService.addTransaction(credit, debit, amount, commission);
+//    }
 
 }
